d25a01da38a9770aef227cf64cab895ad82be11c
charliespt <charliespt@gmail.com>
2014-02-27 17:25:18 -0800

    Initial Commit

---
+++ Hangman/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hangman
+//
+//  Created by Charlie on 2/27/14.
+//  Copyright (c) 2014 Charlie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Hangman/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Hangman
+//
+//  Created by Charlie on 2/27/14.
+//  Copyright (c) 2014 Charlie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
